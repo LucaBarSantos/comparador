9af04a6c852947f53f734625811231b2cbc15f0f
LucaBarSantos <luca.barsantos@gmail.com>
2025-11-29 20:42:50 -0300

    comparador.v

--- comparador.v
+++ comparador.v
@@ -1,31 +1,24 @@
 module comparador(
-    input  [3:0] tentativaA,
-    input  [3:0] senhaA,
-    input  [2:0] tentativaB,
-    input  [2:0] senhaB,
-    input        modoB,   // 0 = comparando A, 1 = comparando B
+    input tentativaA,
+    input A_Pin,
+    input tentativaB,
+    input B_Pin,
+    input modoB,              // 0=A, 1=B
     output reg [1:0] resultado
 );
-
     always @(*) begin
-        if (!modoB) begin
-            // Comparando senha A
-            if (tentativaA > senhaA)
-                resultado = 2'b01; // maior
-            else if (tentativaA < senhaA)
-                resultado = 2'b00; // menor
+        if (modoB == 1'b0) begin
+            // Comparação A
+            if (tentativaA == A_Pin)
+                resultado = 2'b00; // acerto
             else
-                resultado = 2'b10; // igual
-        end
-        else begin
-            // Comparando senha B
-            if (tentativaB > senhaB)
-                resultado = 2'b01;
-            else if (tentativaB < senhaB)
-                resultado = 2'b00;
+                resultado = 2'b01; // erro
+        end else begin
+            // Comparação B
+            if (tentativaB == B_Pin)
+                resultado = 2'b00; // acerto
             else
-                resultado = 2'b10;
+                resultado = 2'b01; // erro
         end
     end
-
 endmodule
